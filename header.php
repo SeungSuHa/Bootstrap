fb3b0217453ac5d91a7a7f42baabf6ba30691821
SeungSuHa <ht106@naver.com>
2020-08-18 17:11:11 +0900

    header

---
+++ header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>엔터트립</title>
+	<!-- 구글 폰트 -->
+	<link
+	<link href=
+	"https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+	rel="stylesheet">
+	<!-- 페비콘 가져오기 -->
+	<link rel="icon" type="image/png" href="imgs/favicon.png">
+	<!-- 아이콘 가져오기 -->
+	<script src="https://kit.fontawesome.com/08acca0d45.js" crossorigin="anonymous"></script>
+	<!-- 부트스트랩 CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- 제이쿼리 -->
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<!-- 부트스트랩 JS -->
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<!-- 헤더 -->
+	<header>
+		<!-- 탑 헤더 -->
+		<div class="container p-2">
+
+			<!-- 그리드 -->
+			<div class="row justify-content-between align-items-center mx-auto ">
+			<!-- 검색창 -->
+			<div class="col-md-4">
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2 rounded-pill" type="text" placeholder="작품을 검색해보세요">
+					<button class="btn btn-danger	my-2 my-sm-0 rounded-circle"><i class="fas fa-search"></i></button>
+				</form>
+			</div>
+			<!-- 로고 -->
+			<div class="logo d-flex justify-content-center align-items-center col-md-4">
+			<a href="#"><img src="imgs/제목 없음1.png" alt="엔터트립"></a>
+			</div>
+			<!-- 로그인 -->
+			<div class="col-md-4 d-flex justify-content-end align-items-center">
+				<div class="mr-2">
+					<button type="button" class="btn btn-danger">로그인</button>
+				</div>
+				<div class="ml-2">
+					<button type="button" class="btn btn-outline-danger">회원가입</button>
+				</div>	
+			</div>
+			</div>
+		</div>
+		<!-- 네비게이션바 -->
+		<nav class="navbar navbar-expand-lg mt-3">
+			<div class="collapse navbar-collapse justify-content-center container">
+				<ul class="navbar-nav text-center">
+					<li class="nav-item">
+						<div class=testing>
+						<a class="nav-link mr-5 px-0 py-0" href="#">작품정보</a>
+						</div>
+					</li>
+					<li class="nav-item mr-5">
+						<a class="nav-link px-0 py-0" href="#">자유게시판</a>
+					</li>
+					<li class="nav-item mr-5">
+						<a class="nav-link px-0 py-0" href="#">리뷰게시판</a>
+					</li>
+					<li class="nav-item mr-5">
+						<a class="nav-link px-0 py-0" href="#">공지사항</a>
+					</li>
+					<li class="nav-item mr-5">
+						<a class="nav-link px-0 py-0" href="#">고객센터</a>
+					</li>
+					<li class="nav-item mr-5">
+						<a class="nav-link px-0 py-0" href="#">이용안내</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+</header>
+
+</body>
+</html>
